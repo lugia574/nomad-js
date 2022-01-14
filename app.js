9d6a55c4ad564fd40859d69c44c3d1a54f530147
lugia574 <66871221+lugia574@users.noreply.github.com>
2022-01-15 00:14:16 +0900

    2022_0114_4.0~4.7 login

--- app.js
+++ app.js
@@ -1,9 +1,30 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
 
-function onLoginSubmit() {
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+
+function onLoginSubmit(event) {
+  event.preventDefault();
   const username = loginInput.value;
-  console.log(username);
+  localStorage.setItem(USERNAME_KEY, username);
+
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+
+  paintGreetings(username);
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
+function paintGreetings(username) {
+  greeting.innerText = `hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+const saverUsername = localStorage.getItem(USERNAME_KEY);
+
+if (saverUsername === null) {
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+  paintGreetings(saverUsername);
+}
