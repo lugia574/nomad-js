c3074d6e458a093851413bdece0f2ef8a8c87716
lugia574 <66871221+lugia574@users.noreply.github.com>
2022-01-14 16:03:28 +0900

    2022_0114_4.0~4.1 event

--- homework/2.8~3.8 Conditionals/src/index.js
+++ homework/2.8~3.8 Conditionals/src/index.js
@@ -1,17 +1,30 @@
-function handleBackgroundColorEvent() {
-  const intFrameWidth = window.innerWidth;
-  const wrap = document.querySelector(".wrap");
-  const color = wrap.classList.item(1);
-  if (intFrameWidth > 900) {
-    wrap.classList.remove(color);
-    wrap.classList.add("yellow");
-  } else if (intFrameWidth > 600 && intFrameWidth <= 900) {
-    wrap.classList.remove(color);
-    wrap.classList.add("violet");
-  } else {
-    wrap.classList.remove(color);
-    wrap.classList.add("blue");
-  }
-}
-
-window.addEventListener("resize", handleBackgroundColorEvent);
+const windowEvent = {
+  defultBgColorEvent: function defultBgColorEvent() {
+    const intFrameWidth = window.innerWidth;
+    const wrap = document.querySelector(".wrap");
+    if (intFrameWidth > 900) {
+      wrap.classList.add("yellow");
+    } else if (intFrameWidth > 600 && intFrameWidth <= 900) {
+      wrap.classList.add("violet");
+    } else {
+      wrap.classList.add("blue");
+    }
+  },
+  resizeBgColorEvent: function resizeBgColorEvent() {
+    const intFrameWidth = window.innerWidth;
+    const wrap = document.querySelector(".wrap");
+    const color = wrap.classList.item(1);
+    if (intFrameWidth > 900) {
+      wrap.classList.remove(color);
+      wrap.classList.add("yellow");
+    } else if (intFrameWidth > 600 && intFrameWidth <= 900) {
+      wrap.classList.remove(color);
+      wrap.classList.add("violet");
+    } else {
+      wrap.classList.remove(color);
+      wrap.classList.add("blue");
+    }
+  },
+};
+windowEvent.defultBgColorEvent();
+window.addEventListener("resize", windowEvent.resizeBgColorEvent);
