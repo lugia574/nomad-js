79cb9f949a6c1c4bfb7dc64f5b85c039567a9d19
lugia574 <66871221+lugia574@users.noreply.github.com>
2022-01-23 12:07:40 +0900

    2022_0123

--- js/background.js
+++ js/background.js
@@ -1,4 +1,10 @@
 const images = ["0.jpg", "1.jpg", "2.jpg"];
+// const images = [
+//   "lofi-lofigirl.gif",
+//   "lofi-lofigirl2.gif",
+//   "lofi-lofigirl3.gif",
+//   "lofi-lofigirl4.gif",
+// ];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
